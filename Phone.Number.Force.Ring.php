fc3188f133dd1828fcee12b0480aa4dd38f779ee
MiloszKaczmarczyk <miloszkaczmarczyk2001@gmail.com>
2017-07-13 16:37:04 +0100

    Add files via upload

--- Phone.Number.Force.Ring.php
+++ Phone.Number.Force.Ring.php
@@ -11,21 +11,37 @@ and open the template in the editor.
     </head>
     <body>
         <form action ="#" method ="post">
-            Please input phone number you want to dial here:
-            <input type ="number" name ="PhoneNumber" min = "99999999" max = "9999999999" >
-            <input type ="submit" value ="Submit">
-            <input type ="reset">
+	    Please enter your user details:<br>
+	    <input type ="text" name ="UserName" value = "Your username here"><br>
+	    <input type ="password" name ="UserPass" value ="******************"><br>
+            Please input phone number you want to dial here:<br>
+            <input type ="number" name ="PhoneNumber" min = "99999999" max = "9999999999" ><br>
+            <input type ="submit" value ="Submit"><br>
+            <input type ="reset"><br>
         </form>
     <?php
-   
+
     if($_POST["PhoneNumber"] != 0){
         print "The phone number you want to dial  is: ";
         print $_POST["PhoneNumber"];
 	$dial = $_POST["PhoneNumber"];
-	$ch = curl_init("http://192.168.1.107/command.htm?number=$dial");
+	$username = $_POST["UserName"];
+	$password = $_POST["UserPass"];
+	$cookie ="cookie.txt";
+	$postdata = "user=".$username."&password=".$password;
+	$ch = curl_init();
+	curl_setopt ($ch, CURLOPT_URL, "http://192.168.1.107/command.htm?number=$dial");
+	curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
+	curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 0);
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt ($ch, CURLOPT_COOKIEJAR, $cookie);
+	curl_setopt ($ch, CURLOPT_REFERER, "http://192.168.1.107/command.htm?number=$dial"); 
+	curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata);
+	curl_setopt ($ch, CURLOPT_POST, 1);
+	curl_error($ch);
 	curl_exec($ch);
 	curl_close($ch);
-	curl_error($ch);
 	}else{
 	print "Please input a number";
 	}
