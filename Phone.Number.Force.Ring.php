c3f13be6faf361392c6fb240c8411b55f2cfa7c3
MiloszKaczmarczyk <miloszkaczmarczyk2001@gmail.com>
2017-07-13 12:44:46 +0100

    Add files via upload

---
+++ Phone.Number.Force.Ring.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Phone Number Forced Ring</title>
+    </head>
+    <body>
+        <form action ="#" method ="post">
+            Please input phone number you want to dial here:
+            <input type ="number" name ="PhoneNumber" min = "99999999" max = "9999999999" >
+            <input type ="submit" value ="Submit">
+            <input type ="reset">
+        </form>
+    <?php
+   
+    if($_POST["PhoneNumber"] != 0){
+        print "The phone number you want to dial  is: ";
+        print $_POST["PhoneNumber"];
+	$dial = $_POST["PhoneNumber"];
+	$ch = curl_init("https://192.168.1.107/command.htm?number=07742370981");
+	curl_exec($ch);
+	curl_close($ch);
+	}else{
+	print "Please input a number";
+	}
+
+    ?>
+        
+    </body>
+</html>
