8692f9ba6603c8e9f546dd62ec9abbdf97f70f7b
MiloszKaczmarczyk <miloszkaczmarczyk2001@gmail.com>
2017-07-14 14:18:20 +0100

    Add files via upload

--- Phone.Number.Force.Ring.php
+++ Phone.Number.Force.Ring.php
@@ -16,16 +16,16 @@
 	if(empty($_POST)) { 
 	   ?> <center> <form action ="#" method ="post" name ="CALLFORM">
 	    <b> Please enter your user details:</b><br>
-	    <input type ="text" name ="CALLFORM-UserName" value = "Your username here"><br>
-	    <input type ="password" name ="CALLFORM-UserPass" value ="******************"><br>
+	    <input type ="text" style="background-color: #2D6CA3"  name ="CALLFORM-UserName" value = "Your username here"><br>
+	    <input type ="password" style="background-color: #2D6CA3" name ="CALLFORM-UserPass" value ="Your password here"><br>
             <b>Please input phone number you want to dial here:</b><br>
-            <input type ="number" name ="CALLFORM-PhoneNumber" min = "99999999" max = "9999999999" ><br>
-            <input type ="submit" name ="CALLFORM-action"  value ="Submit"><br>
-            <input type ="reset"></center><br></form>
+            <input type ="number" style="background-color: #2D6CA3" name ="CALLFORM-PhoneNumber" min = "99999999" max = "9999999999" ><br>
+            <input type ="submit" class="btn btn-primary" name ="CALLFORM-action"  value ="Submit"><br>
+            <input type ="reset" class="btn btn-primary" > </center><br></form>
 	<?php }else{
 		?> <form   action = "#" method ="post" name ="CANCELFORM">
 		   <center><b>Cancel the dial</b><br>
-		   <input type ="submit" name ="CANCELFORM-action" value ="Cancel"
+		   <input type ="submit" class="btn btn-primary" name ="CANCELFORM-action" value ="Cancel"
         	   $username = $_POST["CALLFORM-UserName"];
 	           $password = $_POST["CALLFORM-UserPass"];
 		   </center></form>
@@ -34,8 +34,8 @@
 	
     if($_POST["CALLFORM-PhoneNumber"] != 0){
 	print ("<p align ='center'><b>". $_POST["CALLFORM-PhoneNumber"]. "</b></p>");
-	$dial = $_POST["CALLFORM-PhoneNumber"];
-        $username = $_POST["CALLFORM-UserName"];
+        $dial = $_POST["CALLFORM-PhoneNumber"];
+	$username = $_POST["CALLFORM-UserName"];
         $password = $_POST["CALLFORM-UserPass"];
 	setcookie("Username", $username, time() + 60);
 	setcookie("Password", $password, time() + 60);
@@ -49,7 +49,7 @@
 	$info = curl_getinfo($ch);
 	curl_close($ch);
 	}else{
-                print ("<p align='center'><b>Please input a number</b></p>");
+                print ("<p align='center'><b><mark>Please input a number</mark></b></p>");
 	}
      if (isset($_POST["CANCELFORM-action"])) {
         $username = $_COOKIE["Username"];
@@ -63,7 +63,7 @@
         $output2 = curl_exec($ch2);
         $info2 = curl_getinfo($ch2);
         curl_close($ch2);
-        print("<p align ='center'><b>Call cancelled</b></p>");
+        print("<p align ='center'><b><mark>Call cancelled</mark></b></p>");
         }
 	?>
 			 
